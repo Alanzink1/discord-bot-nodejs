b337f8bcffc866077504135a27cc1ff1799b31a0
Dev Alan Rodrigues <alanrod37831@gmail.com>
2023-03-10 22:19:39 -0300

    discord-bot nodejs[

---
+++ commands/help.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+// inside a command, event listener, etc.
+const exampleEmbed = new EmbedBuilder()
+  .setColor("Orange")
+  .setTitle("Comandos Dave")
+  .setURL("https://www.youtube.com/watch?v=C3K6QX-xp8Q")
+  .setDescription("Dave, seu melhor amigo de todas as calls e conversas!")
+  .setThumbnail(
+    "https://s2.glbimg.com/NrMU35-3lxQbfTs0zmA9sLxOBjM=/0x0:571x561/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/B/8/HpPF3PRmC8r1MlL9wN1g/manoelgomes.jpg"
+  )
+  .addFields(
+    { name: "\u200B", value: "\u200B" },
+    { name: "/ping", value: 'Responde "Pong!"', inline: true },
+    { name: "/help", value: "Relembrar Comandos do Dave", inline: true }
+  )
+  .setImage(
+    "https://static.wixstatic.com/media/368834_fe09ceaea3ac4992b8d81bfed572de23~mv2.png/v1/fit/w_768%2Ch_768%2Cal_c%2Cq_80,enc_auto/file.jpg"
+  )
+  .setTimestamp()
+  .setFooter({
+    text: "WarTech Bots",
+    iconURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHGbCzMNLEN99K7woqj02BN5K8g3wVI0XWYw&usqp=CAU",
+  });
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Relembrar Comandos do Dave'"),
+
+  async execute(interaction) {
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
+};
---
+++ deploy-commands.js
@@ -0,0 +1,46 @@
+const { REST, Routes } = require("discord.js");
+
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+// Importação dos Comandos
+const fs = require("node:fs");
+const path = require("node:path");
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+const commands = [];
+
+for (const file of commandFiles) {
+
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+
+}
+
+// Instância REST
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+// deploy
+(async () => {
+
+    try {
+
+        console.log(`Resetando ${commands.length} comandos...`)
+
+        // PUT
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            {body: commands}
+        );
+            console.log("Comandos Registrados com Sucesso!")
+
+    } catch(e){
+
+        console.error(e);
+
+    }
+
+})();
